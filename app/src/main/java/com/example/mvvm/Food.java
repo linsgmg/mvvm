625d5cda8f49eca57d68a67dc84de79687da9ea5
linshengming <1457815431@qq.com>
2019-05-13 15:54:26 +0800

    mvvm列表的展示和item点击事件的实现处理

--- app/src/main/java/com/example/mvvm/Food.java
+++ app/src/main/java/com/example/mvvm/Food.java
@@ -1,37 +1,50 @@
 package com.example.mvvm;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.databinding.BindingAdapter;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 /**
  * Created by lin on 2019/5/10.
  */
-public class Food {
+public class Food extends BaseObservable {
     private String description;
     private String img;
     private String keywords;
     private String summary;
+    private int index;
 
     public Food() {
     }
 
-    public Food(String description, String img, String keywords, String summary) {
+    //    public Food(String description, String img, String keywords, String summary) {
+//        this.description = description;
+//        this.img = img;
+//        this.keywords = keywords;
+//        this.summary = summary;
+//    }
+    public Food(String description, String img, String keywords, int index) {
         this.description = description;
         this.img = img;
         this.keywords = keywords;
-        this.summary = summary;
-    }
-    public Food(String description, String img, String keywords) {
-        this.description = description;
-        this.img = img;
-        this.keywords = keywords;
+        this.index = index;
     }
 
     @BindingAdapter("img")
     public static void loadInternetImage(ImageView iv, String img) {
         Picasso.with(iv.getContext()).load(img).into(iv);
+    }
+
+    public void onItemClick(View view) {
+        if (index % 2 == 1) {
+            Toast.makeText(view.getContext(), getDescription(), Toast.LENGTH_SHORT).show();
+        }
+        setImg("http://img2.imgtn.bdimg.com/it/u=3157709739,1819601757&fm=26&gp=0.jpg");
     }
 
     public String getDescription() {
@@ -42,12 +55,14 @@ public class Food {
         this.description = description;
     }
 
+    @Bindable
     public String getImg() {
         return img;
     }
 
     public void setImg(String img) {
         this.img = img;
+        notifyPropertyChanged(com.example.mvvm.BR.img);
     }
 
     public String getKeywords() {
@@ -64,5 +79,13 @@ public class Food {
 
     public void setSummary(String summary) {
         this.summary = summary;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
     }
 }
--- app/src/main/java/com/example/mvvm/Main3Activity.java
+++ app/src/main/java/com/example/mvvm/Main3Activity.java
@@ -45,7 +45,7 @@ public class Main3Activity extends AppCompatActivity {
 
     private void initData() {
         OkHttpClient client = new OkHttpClient.Builder().build();
-        Request request = new Request.Builder().url("http://192.168.5.114/users").build();
+        Request request = new Request.Builder().url("http://localhost:3000/users").build();
         client.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
@@ -83,7 +83,7 @@ public class Main3Activity extends AppCompatActivity {
                 String img = item.getString("img");
                 String keywords = "【关键词】 "+item.getString("keywords");
 //                String summary = item.getString("summary");
-                foods.add(new Food(description, img, keywords));
+                foods.add(new Food(description, img, keywords,i));
             }
             mHandler.sendEmptyMessage(0);
         } catch (JSONException e) {
